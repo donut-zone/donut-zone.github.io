5714f81cdaaac191909b16c8471f9b6039e6a716
sleep9ull <sleep9ull@gmail.com>
2024-01-27 22:40:13 +0800

    change astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://donut-zone.github.io',
-  base: '/donut-zone.github.io',
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+})
